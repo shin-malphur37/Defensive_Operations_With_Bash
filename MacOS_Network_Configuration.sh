e81d097e015493a610e95c91e118c74557dd1481
King Joshua K Sanford Israel <jsanfo9@wgu.edu>
2025-07-30 02:47:50 -0500

    Update MacOS_Network_Configuration.sh

--- MacOS_Network_Configuration.sh
+++ MacOS_Network_Configuration.sh
@@ -5,21 +5,41 @@
 sudo ifconfig {interface name} down #repeat as many times as needed for multiple interfaces
 
 
-# set DNS servers & configure ip address using quad9 ipv4/ipv6
+# set DNS servers & configure ip address using quad9 ipv4/ipv6 Wi-fi
 sudo networksetup -setdnsservers Wi-fi 9.9.9.9 149.112.112.112 2620:fe::fe 2620:fe::9
+sudo networksetup -setsearchdomains Wi-Fi {insert search domain}
+sudo ipconfig set en1 INFORM {insert intended ip address} {dns mask} {router} #en1 is the defualt
+
+# set DNS servers & configure ip address using quad9 ipv4/ipv6 Ethernet
+sudo networksetup -setdnsservers Ethernet 9.9.9.9 149.112.112.112 2620:fe::fe 2620:fe::9
+sudo networksetup -setsearchdomains Ethernet {insert search domain}
+sudo ipconfig set en1 INFORM {insert intended ip address} {dns mask} {router}
+
+# set DNS servers & configure ip address using quad9 ipv4/ipv6 Thunderbolt\ Bridge
+sudo networksetup -setdnsservers Thunderbolt\ Bridge 9.9.9.9 149.112.112.112 2620:fe::fe 2620:fe::9
+sudo networksetup -setsearchdomains Thunderbolt\ Bridge {insert search domain}
+sudo ipconfig set en1 INFORM {insert intended ip address} {dns mask} {router}
+
+# set DNS servers & configure ip address using cloudfare ipv4/ipv6 Wi-fi
+sudo networksetup -setdnsservers Wi-fi 1.1.1.3 1.0.0.3 2606:4700:4700::1113 2606:4700:4700::1003 #blocks malware and adult content
 sudo networksetup -setsearchdomains Wi-Fi {insert search domain}
 sudo ipconfig set en1 INFORM {insert intended ip address} {dns mask} {router}
 
-# set DNS servers & configure ip address using cloudfare ipv4/ipv6
-sudo networksetup -setdnsservers Wi-fi 1.1.1.3 1.0.0.3 2606:4700:4700::1113 2606:4700:4700::1003
-sudo networksetup -setsearchdomains Wi-Fi {insert search domain}
+# set DNS servers & configure ip address using cloudfare ipv4/ipv6 Ethernet
+sudo networksetup -setdnsservers Ethernet 1.1.1.3 1.0.0.3 2606:4700:4700::1113 2606:4700:4700::1003 #blocks malware and adult content
+sudo networksetup -setsearchdomains Ethernet {insert search domain}
+sudo ipconfig set en1 INFORM {insert intended ip address} {dns mask} {router}
+
+# set DNS servers & configure ip address using cloudfare ipv4/ipv6 Thunderbolt\ Bridge
+sudo networksetup -setdnsservers Thunderbolt\ Bridge 1.1.1.3 1.0.0.3 2606:4700:4700::1113 2606:4700:4700::1003 #blocks malware and adult content
+sudo networksetup -setsearchdomains Thunderbolt\ Bridge {insert search domain}
 sudo ipconfig set en1 INFORM {insert intended ip address} {dns mask} {router}
 
 # turning off ethernet if only using wifi
 sudo networksetup -setv4off Ethernet
 sudo networksetup -setv4off Thunderbolt\ Bridge
 
-# turn off ipv6 
+# turn off ipv6 services
 sudo networksetup -setv6off Ethernet
 sudo networksetup -setv6off Wi-fi
 sudo networksetup -setv6off Thunderbolt\ Bridge
